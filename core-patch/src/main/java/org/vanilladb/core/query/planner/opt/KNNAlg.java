9931ce3a64b6572fb150480ae63a87e00d812480
vichuang <d10631228@gapps.fg.tp.edu.tw>
2023-06-11 07:20:47 +0800

    findKNN almost done

--- core-patch/src/main/java/org/vanilladb/core/query/planner/opt/KNNAlg.java
+++ core-patch/src/main/java/org/vanilladb/core/query/planner/opt/KNNAlg.java
@@ -19,7 +19,7 @@ import org.vanilladb.core.util.ByteHelper;
 public class KNNAlg{
     // Bench Setting
     String tblName;
-    private int numDimension, numItems;
+    private int numDimension, numItems, numNeighbors;
 
     // Hyper Parameters
     private int numGroups = 3;
@@ -27,18 +27,15 @@ public class KNNAlg{
 
     // Utils
 	private KNNHelper knnHelper;
-    
-    // KMeans Utils (function KMeans is synchonized)
 	private static int curItems = 0;
 	private Random random = new Random();
     VectorConstant[] groupCenter = new VectorConstant[numGroups];
-    int[] groupId = new int[numItems];
-
     
-    public KNNAlg(String _tblName, int _numDimension, int _numItems) {
+    public KNNAlg(String _tblName, int _numDimension, int _numItems, int _numNeighbors) {
         tblName = _tblName;
         numDimension = _numDimension;
         numItems = _numItems;
+		numNeighbors = _numNeighbors;
         knnHelper = new KNNHelper(tblName, numDimension);
     }
    
@@ -47,21 +44,64 @@ public class KNNAlg{
         if(curItems == numItems) KMeans(tx);
     }
 
+	public void findKNN(VectorConstant query, Transaction tx) {
+		DistanceFn distFn = new EuclideanFn("vector");
+		distFn.setQueryVector(query);
+		TablePlan p = new TablePlan(tblName, tx);
+
+		// TODO: load groupCenter for once (ONCE is especially important for thread safe)
+
+		// 1. Assign query vector to a group
+		int gid = 0;
+		Double minDist = Double.MAX_VALUE;
+
+		TableScan s = (TableScan) p.open();
+		s.beforeFirst();
+		for(int i = 0; i < numGroups; i++){
+			Double dist = distFn.distance(groupCenter[i]);
+			if(dist < minDist){
+				minDist = dist;
+				gid = i;
+			}
+		}
+		s.close();
+		
+		// 2. Calculate distance between query and all other vectors
+		Constant const_gid = Constant.newInstance(Type.INTEGER, ByteHelper.toBytes(gid));
+		List<RecordId> ridList = knnHelper.queryRecord(const_gid, tx);
+		int vecInGroup = ridList.size();
+
+		Double[] dist = new Double[vecInGroup];
+		int[] id = new int[vecInGroup];
+
+		for(int i=0; i<vecInGroup; i++)
+		{
+			VectorConstant vec = knnHelper.getVec(ridList.get(i), tx);
+			dist[i] = distFn.distance(vec);
+			id[i] = ridList.get(i).id();
+		}
+		
+		// 3. Search top K vector in the group
+        KSmallest(dist, 0, vecInGroup - 1, numNeighbors); // function undone (but almost done)
+		// return List<VectorConstant>
+	}
+
     private void KMeans(Transaction tx) {
         TablePlan p = new TablePlan(tblName, tx);
 		DistanceFn distFn = new EuclideanFn("vector");
 		Double prev_error = Double.MAX_VALUE, error;
+		int[] groupId = new int[numItems];
 
 		KMeans_init(p, tx);
 
         while(true) {
-			KMeans_update(p, distFn, tx);
-            error = KMeans_calError(p, tx);
+			KMeans_update(p, groupId, distFn, tx);
+            error = KMeans_calError(p, groupId, tx);
             if(error - prev_error <= tolerence) break;
             prev_error = error;
 		}
 
-		KMeans_store(p, tx);
+		KMeans_store(p, groupId, tx);
     }
 
     private void KMeans_init(TablePlan p, Transaction tx) {
@@ -91,7 +131,7 @@ public class KNNAlg{
             groupCenter[i] = knnHelper.getVec(ridList.get(i), tx);
     }
 
-	private void KMeans_update(TablePlan p, DistanceFn distFn, Transaction tx) {
+	private void KMeans_update(TablePlan p, int[] groupId, DistanceFn distFn, Transaction tx) {
 		// 1. Recluster by finding the nearest group center for each vector
 		TableScan s = (TableScan) p.open();
 		s.beforeFirst();
@@ -138,7 +178,7 @@ public class KNNAlg{
 		}
 	}
 
-	private Double KMeans_calError(TablePlan p, Transaction tx) {
+	private Double KMeans_calError(TablePlan p, int[] groupId, Transaction tx) {
 		Double error = 0.0;
 		TableScan s = (TableScan) p.open();
 		s.beforeFirst();
@@ -158,7 +198,7 @@ public class KNNAlg{
 		return error;
 	}
 
-	private void KMeans_store(TablePlan p, Transaction tx) {
+	private void KMeans_store(TablePlan p, int[] groupId, Transaction tx) {
 		// Storing groupId and groupCenter back to tables.
 
         TableScan s = (TableScan) p.open();
@@ -173,9 +213,40 @@ public class KNNAlg{
 
 		// TODO: storing groupCenter back
     }
-}
 
-/*
-public Constant queryGroup(RecordId recordId, Transaction tx)
-public List<RecordId> queryRecord(Constant groupId, Transaction tx)
- */
+	
+    private void KSmallest(Double[] arr, int low, int high, int k) {
+		// Using quickselect algorithm
+        int pivot = Partition(arr, low, high);
+  
+        // if (pivot == k - 1), end.
+  
+        if (pivot < k - 1)
+            KSmallest(arr, pivot + 1, high, k);
+  
+        else if (pivot > k - 1)
+            KSmallest(arr, low, pivot - 1, k);
+    }
+
+	private int Partition(Double[] arr, int low, int high) {
+        // Move smaller elements to the left of pivot
+        // and higher elements to the right
+    
+        Double pivot = arr[high];
+		int pivotloc = low;
+        for (int i = low; i <= high; i++) {
+            if (arr[i] < pivot) {
+                Double temp = arr[i];
+                arr[i] = arr[pivotloc];
+                arr[pivotloc] = temp;
+                pivotloc++;
+            }
+        }
+  
+        Double temp = arr[high];
+        arr[high] = arr[pivotloc];
+        arr[pivotloc] = temp;
+  
+        return pivotloc;
+    }
+}
