eaa6afe28abe7794a13812b9b5252ab35240544d
Wilbert Harriman <wilbert26harriman@gmail.com>
2023-06-12 14:50:30 +0800

    Pseudorandom query

--- bench/src/main/java/org/vanilladb/bench/benchmarks/ann/rte/AnnParamGen.java
+++ bench/src/main/java/org/vanilladb/bench/benchmarks/ann/rte/AnnParamGen.java
@@ -9,12 +9,26 @@ import org.vanilladb.core.sql.VectorConstant;
 
 public class AnnParamGen implements TxParamGenerator<AnnTransactionType> {
 
+    private static final int NUM_QUERY = 10000;
+    private static final long QUERY_SEED = 42;
+
+    private VectorConstant[] queryPool = new VectorConstant[NUM_QUERY];
     private VectorConstant query;
+
+    public AnnParamGen() {
+        VectorConstant.seed(QUERY_SEED);
+
+        for (int i = 0; i < queryPool.length; i++) {
+            queryPool[i] = new VectorConstant(AnnBenchConstants.NUM_DIMENSION);
+        }
+    }
 
     @Override
     public AnnTransactionType getTxnType() {
         return AnnTransactionType.ANN;
     }
+
+    private int idx = 0;
 
     @Override
     public Object[] generateParameter() {
@@ -25,8 +39,9 @@ public class AnnParamGen implements TxParamGenerator<AnnTransactionType> {
 		// =====================
         paramList.add(AnnBenchConstants.NUM_DIMENSION);
 
-        // Generate a query vector
-        query = new VectorConstant(AnnBenchConstants.NUM_DIMENSION);
+        // Get a query vector from the query pool
+        query = queryPool[idx];
+        idx = (idx + 1) % NUM_QUERY;
         
         for (int i = 0; i < AnnBenchConstants.NUM_DIMENSION; i++) {
             paramList.add(query.get(i));
